3814a0e151d32d6ccc2c5482ebcaaaecfc4c0c25
erenturkk0 <74346255+erenturkk0@users.noreply.github.com>
2020-12-16 13:16:32 +0300

    Update sayac.js

--- komutlar/sayac.js
+++ komutlar/sayac.js
@@ -1,36 +1,64 @@
-const Discord = require('discord.js');
-const db = require('quick.db')
+const Discord = require("discord.js");
+const fs = require("fs");
+const ayarlar = require("../ayarlar.json");
 
-exports.run = async (client, message, args, ) => {
-  
-  let sayi = args[0];
-  let kanal = message.mentions.channels.first();
-  
-  if (!sayi) return message.channel.send(':negative_squared_cross_mark: **| Lütfen Bir Sayı Belirtiniz.**')
-  if (!kanal) return message.channel.send(':negative_squared_cross_mark: **| Lütfen Kanal Belirtiniz.**')
-  
-  db.set(`sayac_${message.guild.id}`, sayi);
-  db.set(`sayacK_${message.guild.id}`, kanal.id);
-  
+exports.run = async (client, message, args) => {
+  const db = require("quick.db");
 
-  message.channel.send
-new Discord.MessageEmbed()
-                        (`
-                        **¦-======= Avonix Sayaç =======¦
-                          ¦> :rocket: Sayaç Aktif Edildi.
-                          ¦> :rocket: **${args[0]}** Olarak Güncelledim!
-                          ¦> :rocket: Kayıt Kanalını **${kanal}** Olarak Güncelledim!
-                          ¦> :rocket: Belirdeğiniz Sayı Kadar Kişi Olmaya Son **NaN** Kişi Kaldı.
-                          ¦======= Avonix Sayaç =======-¦`)
-}
+  let prefix = ayarlar.prefix;
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
+
+  if (!args[0]) {
+    return message.reply("Lütfen ayarlamak istediğiniz sayıyı yazınız");
+  }
+
+  if (args[0] === "kapat") {
+    if (db.has(`sayac_${message.guild.id}`) === true) {
+      db.delete(`sayac_${message.guild.id}`);
+
+      if (db.has(`sKanal_${message.guild.id}`) === true) {
+        db.delete(`sKanal_${message.guild.id}`);
+        message.channel.send("Sayaç kanalı ve sayaç başarıyla kaldırıldı");
+        return;
+      }
+
+      message.channel.send("Sayaç kaldırıldı.");
+      return;
+    }
+    message.channel.send(`Sayaç ayarlanmamış.`);
+    return;
+  }
+
+  if (isNaN(args[0])) {
+    return message.reply("Sadece sayı!");
+  }
+
+  if (args[0] <= message.guild.memberCount) {
+    const embed = new Discord.MessageEmbed();
+    return message.reply("Lütfen sunucu sayısından daha yüksek bir değer girin!" );
+  }
+
+  db.set(`sayac_${message.guild.id}`, args[0]);
+
+  const embed = new Discord.MessageEmbed()
+    .setTimestamp()
+    .setAuthor(`
+Sayaç başarıyla ayarlandı: **${args[0]}**
+Sayaç kapatmak isterseniz **a!sayaç kapat** yazmanız yeterlidir.
+Sayaç kanalı için a!sayaç-kanal-ayarla #kanal
+`);
+  message.channel.send(embed);
+};
+
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ["sayaç-ayarla"],
-  permLevel: 3
-}
+  guildOnly: true,
+  aliases: ["sayacayarla", "sayac", "sayaç"],
+  permLevel: 0
+};
+
 exports.help = {
-  name: "sayaç",
-  description: "Sayaç ayarlama komutudur.",
-  usage: "sayaç <sayi> <#kanal>"
-}
+  name: "sayaç-ayarla",
+  description: "Sayacı ayarlar.",
+  usage: "saya-çayarla "
+};
