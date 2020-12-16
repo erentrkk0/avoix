ab1873a1ac8f9bc31d838b8aa6ec51a036833d71
erenturkk0 <74346255+erenturkk0@users.noreply.github.com>
2020-12-16 20:57:32 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   const davet = new Discord.MessageEmbed()
   .setColor("RANDOM")
   .setTitle("Beni Sunucuna Ekle ❤️")
-  .setDescription("[__**Davet Et**__](https://discord.boats/bot/723152948548468828) \n [__**Destek Sunucusu**__](https://discord.gg/FBxxdYk) \n [__**Bot Site**__](https://tr.avonixbot.tk)")
+  .setDescription("[__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=781194108885925948&scope=bot&permissions=8) \n [__**Destek Sunucusu**__](https://discord.gg/FBxxdYk) \n [__**Bot Site**__](https://tr.avonixbot.tk)")
   message.channel.send(davet)
 }
 
