dd02dc6714b3a611725f15ebd9c9c5661e0b44b2
erenturkk0 <74346255+erenturkk0@users.noreply.github.com>
2020-12-16 23:48:07 +0300

    Update yardim2.js

--- komutlar/yardim2.js
+++ komutlar/yardim2.js
@@ -9,7 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("Avonix Yardım.")
 .setTitle("<a:rocket:742698148329291826> Avonix'in yardım menüsüne hoşgeldin! <a:rocket:742698148329291826>")
  .setTimestamp()
-.setDescription("<a:rocket:742698148329291826> **a!korumalar ** = __Korumaları Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!sunucu-tema ** = __Sunucu Temalarını Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!ban-sistem ** = __Ban Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!mute-sistem ** = __Mute Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!sayaç ** = __Sayaç Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!otorol ** = __Otorol Sistemini Görüntülersiniz.__ \n <a:rocket:750065080741134407> **a!ototag** = Sunucuya Giren Üyelere Ototag Verir<a:rocket:749525084586115153> \n <a:rocket:750065080741134407> **a!davetsistem** = Davet Sistemi Yardim Menusunu Gosterir <a:rocket:749525084586115153>")    
+.setDescription("<a:rocket:742698148329291826> **a!korumalar ** = __Korumaları Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!sunucu-tema ** = __Sunucu Temalarını Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!ban-sistem ** = __Ban Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!mute-sistem ** = __Mute Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!sayaç ** = __Sayaç Sistemini Görüntülersiniz.__ \n <a:rocket:742698148329291826> **a!otorol ** = __Otorol Sistemini Görüntülersiniz.__ \n <a:rocket:750065080741134407> **a!ototag** = Sunucuya Giren Üyelere Ototag Verir<a:rocket:749525084586115153>")    
 .setImage("https://cdn.discordapp.com/attachments/781626350364131352/785164861708697640/standard_2.gif")
 message.channel.send(yardım)
 }
