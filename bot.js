fb915d84f72f122633be272817aa5423a41a25ff
erentrkk0 <68477904+erentrkk0@users.noreply.github.com>
2021-04-20 18:41:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const fs=require('fs');
+require("./util/eventLoader.js")(client);
 const Discord=require("discord.js");
 const client=new Discord.Client();
 const db = require('quick.db')
@@ -45,26 +46,6 @@ app.get('/', (req, res) => res.send("Bot Aktif"))
 app.listen(process.env.PORT, () => console.log('Port ayarlandı: ' + process.env.PORT))
 
 
-
-
-client.on("message", message => {
-  let client = message.client;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
-  let perms = client.yetkiler(message);
-  let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
-  }
-})
 
 
 client.on("ready", () => {
